dd9f1df0f08ff02318f9a5635c5805ef0d10b664
ST10090106 <ST10090106@vcconnect.edu.za>
2023-08-18 07:56:00 +0000

    Add project files.

---
+++ PROG3B_ICE Task 1/ArrayList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG3B_ICE_Task_1
+{
+    class ArrayList
+    {
+        public static void Main(string[] args)
+        {
+
+            // Creating an array list
+            List<string> words = new List<string>();
+            words.Add("programming");
+            words.Add("algorithm");
+            words.Add("sorting");
+            words.Add("arrays");
+            words.Add("ice task");
+            words.Add("static void method");
+
+            
+        }
+
+        // Creating a method for scrammbling the words
+        public string shufflewords(string vocabwords)
+        {
+            char[] chars = new char[vocabwords.Length];
+            Random random = new Random(10000);
+
+            int index = 0;
+
+            // Getting the random number between the number 0 and the length of the word
+            while (vocabwords.Length > 0)
+            {
+                int next = random.Next(0, vocabwords.Length - 1);
+                chars[index] = vocabwords[next];
+                vocabwords = vocabwords.Substring(0, next) + vocabwords.Substring(next + 1);
+
+                index++;
+            }
+
+            // Returning the value
+            return new String(chars);
+
+        }
+
+        // Creating a method that will check the words if it is correct or incorrect
+        public static void Checkword()
+        {
+
+
+        }
+
+    }
+}
---
+++ PROG3B_ICE Task 1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PROG3B_ICE_Task_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void answer_textbox(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Play_button(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Check_button(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
